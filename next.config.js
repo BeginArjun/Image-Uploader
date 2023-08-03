556ee13b0e641f7a1bbfe8b10b7c1274cd68c57c
BeginArjun <jayshrma161@gmail.com>
2023-08-03 22:52:43 +0530

    Added Proxy to Backend REST API

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async rewrites(){
+        return[
+            {
+                source:'/upload',
+                destination:'http://localhost:8080/api/uploads'
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
