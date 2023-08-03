47e22042587a951cd92b28595e61e0ab29f4b9ea
BeginArjun <jayshrma161@gmail.com>
2023-08-03 22:51:01 +0530

    Added Functionality

--- app/components/Upload/index.js
+++ app/components/Upload/index.js
@@ -1,20 +1,48 @@
+'use client'
 import Image from "next/image"
 import UploadIcon from "../../assets/image.svg"
+import axios from "axios"
+const uploadFile=async(file)=>{
+    const Imgform=document.getElementById('Dragform')
+    const formData=new FormData(Imgform)
+    const {data}=await axios.post('/upload',formData,{
+        headers: {
+            "Content-Type": "multipart/form-data",
+          },
+    })
+    console.log(data)
+}
+
 const UploadContainer=()=>{
     return(
-        <div className="border border-dashed border-[#97BEF4] rounded-xl h-[218.903px] w-[338px] p-[0.75rem] bg-[#F6F8FB]">
-            <div className="flex flex-col justify-around items-center h-full ">
+        <div className="">
+            <form encType="multipart/form-data" className="h-full" id="Dragform">
+                <input id="imgDrop" type="file" name="image" alt="" className="hidden"/>
+                <label className="flex flex-col items-center justify-around border border-dashed border-[#97BEF4] rounded-xl h-[218.903px] w-[338px] p-[0.75rem] bg-[#F6F8FB]" 
+                for="imgDrop">
                 <Image src={UploadIcon} alt="upload-icon" className="max-w-[114.131px] max-h-[88.241px]"/>
-                <p className="text-[#BDBDBD] text-xs font-medium -tracking-tighter">Drag & Drop your Image Here</p>
-            </div>
+                <p className="text-xs text-[#BDBDBD] -tracking-tighter">Drag and Drop Image here</p>
+                </label>
+            </form>
         </div>
     )
 }
 
 
+const handleClick=()=>{
+    const input=document.getElementById('imgDrop')
+    input.onchange=(e)=>{
+        console.log(e.target.files)
+        uploadFile(e.target.files[0])
+    }
+    input.click()
+    
+}
+
 const UploadBtn=()=>{
+    
     return(
-        <button className="bg-[#2F80ED] rounded-lg p-2 text-white">Choose a file</button>
+        <button className="bg-[#2F80ED] rounded-lg p-2 text-white" onClick={handleClick}>Choose a file</button>
     )
 }
 
